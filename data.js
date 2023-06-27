460a25043eac6c78a518360feae266cc8b93967d
RadaRish <rada.rish@ya.ru>
2023-06-27 16:42:51 +0300

    загружен тур с инфоточками

---
+++ data.js
@@ -0,0 +1,404 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "терраса",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.04093590427404692,
+        "pitch": 0.009076168933912498,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8577918602469357,
+          "pitch": 0.10735302403643843,
+          "rotation": 0,
+          "target": "1---"
+        },
+        {
+          "yaw": -0.8625227442112386,
+          "pitch": 0.09075311208855119,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -2.3824416648707896,
+          "pitch": 0.058580439182932764,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.110033410088814,
+          "pitch": 0.8877060982966238,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Выпечка</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Свежая выпечка из собственной пекарни &lt;&lt;Домашняя&gt;&gt;: чуду с мясом - 400р,&nbsp; чуду с мясом и картошкой - 350р, чуду с творогом и зеленью - 250р, курица гриль - 650р, хлеб узбекский, лезгинский, самса с мясом, хачапури с творогом, сосиска в тесте, булочки 3-х видов. Доставка платная (недорого) с 10.00 до 20.00 по телефону: 8-928-984-21-35, 8-989-892-33-23.&nbsp;</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "1---",
+      "name": "кухня на террасе",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.9054972056836554,
+        "pitch": -0.03760127129769586,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8434727862331464,
+          "pitch": 0.05941195934201993,
+          "rotation": 0,
+          "target": "2-----"
+        },
+        {
+          "yaw": 2.530852118556613,
+          "pitch": 0.07669447986031486,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.07442366277783385,
+          "pitch": 0.9308078301509983,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Выпечка</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Свежая выпечка из собственной пекарни &lt;&lt;Домашняя&gt;&gt;: чуду с мясом - 400р, чуду с мясом и картошкой - 350р, чуду с творогом и зеленью - 250р, курица гриль - 650р, хлеб узбекский, лезгинский, самса с мясом, хачапури с творогом , сосиска в тесте, булочки 3-х видов. </font><font style=\"vertical-align: inherit;\">Доставка платная (недорого) с 10.00 до 20.00 по телефонам: 8-928-984-21-35, 8-989-892-33-23.&nbsp;</font></font><br>"
+        },
+        {
+          "yaw": -0.2522455067060374,
+          "pitch": 0.5087665781541464,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Напитки</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Разливные напитки, соки (и не только), питьевая вода, энергия. </font><font style=\"vertical-align: inherit;\">Можно заказать в магазине &lt;&lt;Домашний 24&gt;&gt; по номеру: 8-964-001-95-77, 8-989-659-71-70, 8-963-429-82-91 (после 00.00)</font></font></font></font>"
+        }
+      ]
+    },
+    {
+      "id": "2-----",
+      "name": "шезлонги и место для загара",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.0902846564924893,
+        "pitch": 0.03821541839704068,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.887751945622348,
+          "pitch": 0.11991554389316406,
+          "rotation": 0,
+          "target": "1---"
+        },
+        {
+          "yaw": -2.5077502398030163,
+          "pitch": 0.1177946592787027,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.541048396073961,
+          "pitch": 0.3249197370787549,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Бассейн</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Открытый сезонный бассейн</font></font>"
+        },
+        {
+          "yaw": -2.6321330889382804,
+          "pitch": 0.6214981388855207,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Шезлонги</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Шезлонги для отдыха и загара</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "3--",
+      "name": "мангальная зона",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.2694153699896731,
+        "pitch": 0.15170152097203982,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6053014593725692,
+          "pitch": 0.1043057341908753,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.2668937061145087,
+          "pitch": 0.08209011959194967,
+          "rotation": 0,
+          "target": "2-----"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3347186527090091,
+          "pitch": 0.29229088664041214,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Мангал</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Мангальная зона</font></font></font></font>"
+        },
+        {
+          "yaw": -1.031290453873373,
+          "pitch": 0.49163533741184295,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Самовар</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Свежий самоварный чай</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "апартаменты",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.026655937666816953,
+        "pitch": 0.01685574230585729,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3571731337088515,
+          "pitch": 0.09413493400575312,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.92265546701538,
+          "pitch": 0.1119130691664445,
+          "rotation": 0,
+          "target": "5---"
+        },
+        {
+          "yaw": -2.7483303793649707,
+          "pitch": 0.11581236625308478,
+          "rotation": 0,
+          "target": "6--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8054403625096977,
+          "pitch": 0.6049632969770169,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Дополнительное спальное место</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.10838564807074036,
+          "pitch": 0.8088555103148405,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">&lt;&lt;Домашний 24&gt;&gt;</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Разливные напитки, соки (и не только), питьевая вода, энергия, фрукты, овощи, чипсы, бакалея, сушеная рыба, колбасы. Доставка платная, недорого.&nbsp;</font></font></font></font>Можно заказать в магазине &lt;&lt;Домашний 24&gt;&gt; по номеру: 8-964-001-95-77, 8-989-659-71-70, 8-963-429-82-91 (после 00.00)"
+        }
+      ]
+    },
+    {
+      "id": "5---",
+      "name": "кухня в апартаментах",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.07425597117837057,
+        "pitch": -0.0025931911239815975,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20105602712183668,
+          "pitch": 0.17061235073817116,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.45348623998699544,
+          "pitch": 0.6158860276326585,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Сейф</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Ваши ценные вещи в безопасности.</font></font><div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><br></font></font></div>"
+        },
+        {
+          "yaw": -2.2604333435169597,
+          "pitch": 0.6022916219424275,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Выпечка</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\">Свежая выпечка из собственной пекарни &lt;&lt;Домашняя&gt;&gt;: чуду с мясом - 400р, чуду с мясом и картошкой - 350р, чуду с творогом и зеленью - 250р, курица гриль - 650р, хлеб узбекский, лезгинский, самса с мясом, хачапури с творогом , сосиска в тесте, булочки 3-х видов.&nbsp;</font><font style=\"vertical-align: inherit;\">Доставка платная (недорого) с 10.00 до 20.00 по телефонам: 8-928-984-21-35, 8-989-892-33-23.&nbsp;</font><br>"
+        }
+      ]
+    },
+    {
+      "id": "6--",
+      "name": "санузел-душ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.2942155279417804,
+        "pitch": 0.054490278479910614,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08822304698740702,
+          "pitch": 0.18399305978922698,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1949844301967065,
+          "pitch": -0.08767086039114602,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Полный набор полотенец, халат и тапочки</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.6838061096796864,
+          "pitch": 0.026888591492054914,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Горячая вода 24 часа</font></font>",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
